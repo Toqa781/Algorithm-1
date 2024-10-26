979a3f170707c6efaf8e5ce56d54f05b1dae2d0e
Kernashaat203 <147157199+Kernashaat203@users.noreply.github.com>
2024-10-26 20:58:50 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -9,8 +9,20 @@ int recursiveFib(int n){
     else
         return recursiveFib(n-1)+ recursiveFib(n-2);
 }
+int iterativefib(int n) {
+    int fib[n+2];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[n];
+}
 int main() {
-    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    cout<<recursiveFib(3);
-
+    //ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int n;
+    cout << "Enter number : ";
+    cin >>n;
+    cout<<n<<"th recursive fibonacci terms: "<<recursiveFib(n)<< endl;
+    cout << n<<"th iterative(dynamic programming) fibonacci terms: "<<iterativefib(n)<<endl;
 }
